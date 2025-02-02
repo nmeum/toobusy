dfad2fc23e6b6ed9ae0128f28ef1049830093bc5
Sören Tempel <soeren+git@soeren-tempel.net>
2025-02-02 15:38:16 +0100

    Print events in listing group by day

--- src/toobusy/search.scm
+++ src/toobusy/search.scm
@@ -17,7 +17,12 @@
             event-docid
             event-rank
             event-ics
-            event-print))
+            event-start
+            event-end
+            event-before?
+            event-after?
+            event-print
+            group-events))
 
 ;; TODO: If we want/need the file path, then we need to store that in the
 ;; database. However, since multiple VEVENTs may be stored in the same file it
@@ -30,6 +35,30 @@
   (rank  event-rank)
   (ics   event-ics))
 
+(define (%event-tm prop-name event)
+  (let* ((ics-obj  (event-ics event))
+         (property (ics-object-property-ref ics-obj prop-name)))
+    (ics-property-value
+      (ics-property->typed-property property))))
+
+(define (event-start event)
+  (%event-tm "DTSTART" event))
+
+(define (event-end event)
+  (%event-tm "DTEND" event))
+
+(define (event-before? event other)
+  (define (tm->time tm)
+    (car (mktime tm (or (tm:zone tm) ""))))
+
+  (let* ((time1 (tm->time (event-start event)))
+         (time2 (tm->time (event-start other))))
+    (< time1 time2)))
+
+;; Check if the event is on the same date or after.
+(define (event-after? event other)
+  (not (event-before? event other)))
+
 (define* (event-print event #:optional (port (current-output-port)))
   (let* ((ics-obj (event-ics event))
          (summary (ics-object-property-ref ics-obj "SUMMARY")))
@@ -38,6 +67,18 @@
             (event-docid event)
             (event-rank event)
             (ics-property-value summary))))
+
+(define (group-events events)
+  (fold
+    (lambda (event xs)
+      (if (null? xs)
+        (cons (list event) xs)
+        (let ((cur (event-start event))
+              (prv (event-start (caar xs))))
+          (if (eq? (tm:mday cur) (tm:mday prv))
+            (cons (cons event (car xs)) (cdr xs))
+            (cons (list event) xs)))))
+    '() (sort events event-before?)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
